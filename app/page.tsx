3bbce1d2baa157cc26c5b4e5dfbc735101d410ec
Lucas Bezerra <1lcs.bzrr@gmail.com>
2023-01-03 22:30:50 -0300

    feat: visual changes

--- app/page.tsx
+++ app/page.tsx
@@ -49,7 +49,7 @@ const topItemsData: itemType[] = [
 
 export default function HomePage() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Image
         src="https://i.ibb.co/cXbc9NV/Inserir-um-subt-tulo.jpg"
         alt="banner"
@@ -57,68 +57,71 @@ export default function HomePage() {
         height={200}
       />
       <div className="p-4">
-        <RestaurantAvatar />
         <div className="flex flex-row items-center">
+          <RestaurantAvatar />
           <RestaurantInfo />
         </div>
         <div className="flex flex-row">
-          <div className="mt-9 mr-1">
-            <div className="inline bg-gray-800 px-5 py-2 rounded-full cursor-pointer">
-              <MdLocationOn className="inline text-white w-6 h-6 mr-2" />
-              <span className="text-white text-xs">Rua das Flores, 4321</span>
-              <MdExpandMore className="inline text-white w-6 h-6 ml-2" />
-            </div>
+          <div className="mt-9 mr-1 inline bg-gray-800 px-5 py-1 rounded-full cursor-pointer transition-all ease-in-out duration-300 hover:bg-gray-600">
+            <MdLocationOn className="inline text-white w-6 h-6 mr-2" />
+            <span className="text-white text-xs">Rua das Flores, 4321</span>
+            <MdExpandMore className="inline text-white w-6 h-6 ml-2" />
           </div>
-          <div className="mt-9">
-            <div className="inline bg-gray-800 px-5 py-2 rounded-full cursor-pointer">
-              <FaClock className="inline text-white w-5 h-5 mr-2" />
-              <span className="text-white text-xs">Aberto até 23h</span>
-              <MdExpandMore className="inline text-white w-6 h-6 ml-2" />
-            </div>
+          <div className="mt-9 inline bg-gray-800 px-5 py-1 rounded-full cursor-pointer transition-all ease-in-out duration-300 hover:bg-gray-600">
+            <FaClock className="inline text-white w-5 h-5 mr-2" />
+            <span className="text-white text-xs">Aberto até 23h</span>
+            <MdExpandMore className="inline text-white w-6 h-6 ml-2" />
           </div>
         </div>
-        <div className="mt-9">
+
+        <hr className="border border-solid mt-6" />
+
+        <div className="mt-5">
           <TopItems />
         </div>
-        <div className="mt-9">
+        {/* <div className="mt-9">
           <Categories />
-        </div>
+        </div> */}
       </div>
     </div>
   );
 }
 
-function Categories() {
-  return (
-    <div>
-      {categoriesData.map((item, index) => {
-        return (
-          <div
-            key={index}
-            className="flex flex-row mb-5 rounded-sm cursor-pointer transition-all ease-in-out duration-300 hover:bg-gray-200"
-          >
-            <Image
-              src={item.imageUrl}
-              className="rounded-xl"
-              alt="208c90f0-5596-48a4-a1ce-aebb38cf789d"
-              width={500}
-              height={121}
-            />
-          </div>
-        );
-      })}
-    </div>
-  );
-}
+// function Categories() {
+//   return (
+//     <div>
+//       <h3 className="text-2xl font-bold mb-3 text-gray-800">Categorias</h3>
+//       <div>
+//         {categoriesData.map((item, index) => {
+//           return (
+//             <div
+//               key={index}
+//               className="flex flex-row mb-5 rounded-sm cursor-pointer transition-all ease-in-out duration-300 hover:bg-gray-200"
+//             >
+//               <Image
+//                 src={item.imageUrl}
+//                 className="rounded-xl cursor-pointer transition-all ease-in-out duration-300 hover:brightness-75"
+//                 alt="208c90f0-5596-48a4-a1ce-aebb38cf789d"
+//                 width={500}
+//                 height={121}
+//               />
+//             </div>
+//           );
+//         })}
+//       </div>
+//     </div>
+//   );
+// }
 
 function TopItems() {
   return (
     <div>
+      <h3 className="text-2xl font-bold mb-3 text-gray-800">Destaques</h3>
       {topItemsData.map((item, index) => {
         return (
           <div
             key={index}
-            className="flex flex-row mb-5 bg-gray-100 p-2 rounded-sm cursor-pointer transition-all ease-in-out duration-300 hover:bg-gray-200"
+            className="flex flex-row mb-5 border shadow-lg bg-white p-2 rounded-lg cursor-pointer transition-all ease-in-out duration-300 hover:brightness-90"
           >
             <Image
               src={item.picture}
@@ -144,11 +147,11 @@ function TopItems() {
 
 function RestaurantAvatar() {
   return (
-    <div className="absolute -mt-20 flex items-center justify-center h-36 w-36 rounded-full border-solid border-4 border-gray-700 ">
+    <div className="flex items-center justify-center w-32 h-32 rounded-md">
       <Image
         src="https://i.ibb.co/d0MYCmv/Design-sem-nome.jpg"
         alt="208c90f0-5596-48a4-a1ce-aebb38cf789d"
-        className="rounded-full"
+        className="rounded-md"
         width={200}
         height={200}
       />
@@ -158,17 +161,15 @@ function RestaurantAvatar() {
 
 function RestaurantInfo() {
   return (
-    <div className="mt-20">
-      <div className="flex flex-col">
-        <div className="flex flex-row mt-2">
-          <div className="font-bold text-xl mr-3">Quintal do Hambúrguer</div>
-          <FaStar className="text-yellow-300 w-6 h-6" />
-          <FaStar className="text-yellow-300 w-6 h-6" />
-          <FaStar className="text-yellow-300 w-6 h-6" />
-          <FaStar className="text-yellow-300 w-6 h-6" />
-          <FaStarHalf className="text-yellow-300 w-6 h-6 mr-2" />
-        </div>
-        <p className="text-gray-700 text-base">Hamburgueria</p>
+    <div className="ml-3">
+      <div className="font-bold text-xl">Quintal do Hambúrguer</div>
+      <p className="text-gray-700 text-base">Hamburgueria</p>
+      <div className="flex flex-row mt-3">
+        <FaStar className="text-yellow-300 w-6 h-6" />
+        <FaStar className="text-yellow-300 w-6 h-6" />
+        <FaStar className="text-yellow-300 w-6 h-6" />
+        <FaStar className="text-yellow-300 w-6 h-6" />
+        <FaStarHalf className="text-yellow-300 w-6 h-6 mr-2" />
       </div>
     </div>
   );

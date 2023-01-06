67e9e8d81a280ca561b0c470582b290172cddf5b
Lucas Bezerra <testesautomacao2021@gmail.com>
2023-01-06 17:20:36 -0300

    feat: navbar e sidebar adicionados ao painel admin

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,17 +9,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-
-        "white-300": "#F7F7F7", 
+        "white-300": "#F7F7F7",
 
         "gray-300": "#E2E2E2",
         "gray-400": "#838383",
         "gray-500": "#727272",
         "gray-700": "#3A3A3A",
         "gray-600": "#454545",
+        "gray-800": "#282828",
 
         "green-100": "#1FA262",
-      }
+      },
     },
   },
   plugins: [],

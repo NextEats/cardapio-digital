009a9413793a1437419306eedc775fc2decac227
Eduardo Barroso <barrosoeduardo64@gmail.com>
2023-01-10 23:40:36 -0300

    feat: horário de abertura do restaurante

--- src/components/home/Header/index.tsx
+++ src/components/home/Header/index.tsx
@@ -5,8 +5,11 @@ import { useEffect, useState } from "react";
 import { api } from "../../../server/api";
 import { IRestaurant } from "../../../types/home";
 import { GetServerSideProps } from "next";
+import Link from "next/link";
+import { format, getHours } from "date-fns";
 
-export default function Header( { restaurant }: { restaurant: IRestaurant } ) {
+
+export default function Header({ restaurant }: { restaurant: IRestaurant }) {
   return (
     <div className="">
       <div className="flex flex-row items-center px-4 pt-4">
@@ -41,7 +44,7 @@ export default function Header( { restaurant }: { restaurant: IRestaurant } ) {
   );
 }
 
-function RestaurantAvatar( ) {
+function RestaurantAvatar() {
   return (
     <div className="flex items-center justify-center w-32 h-32 rounded-md">
       <Image
@@ -58,24 +61,68 @@ function RestaurantAvatar( ) {
 function RestaurantInfo({ restaurant }: { restaurant: IRestaurant }) {
 
   const [userPosition, setUserPosition] = useState<{ latitude: number; longitude: number }>({ latitude: 0, longitude: 0 });
+  const { latitude, longitude, type, name, schedules } = restaurant
 
-  
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
-        (position) => { setUserPosition({ latitude: position.coords.latitude, longitude: position.coords.longitude }); },
-        (error) => console.error(error),
-        { enableHighAccuracy: true }
+      (position) => { setUserPosition({ latitude: position.coords.latitude, longitude: position.coords.longitude }); },
+      (error) => console.error(error),
+      { enableHighAccuracy: true }
     );
-}, []);
+  }, []);
 
-console.log(restaurant)
-const restaurantPosition = { latitude: restaurant.latitude , longitude: restaurant.longitude };
-const distance = getDistance(userPosition, restaurantPosition) / 1000; // Distance in km
+  const restaurantPosition = { latitude, longitude };
+  const distance = getDistance(userPosition, restaurantPosition) / 1000; // Distance in km
+
+  function getDailySchadule() {
+    const weekDay = format(new Date(), 'eeee')
+    console.log(schedules)
+
+    switch (weekDay) {
+      case "Monday":
+        return ` ${schedules.monday} `
+        break
+      case "Tuesday":
+        return closedOrOpened(schedules.tuesday.opensAt, schedules.tuesday.closesAt)
+        return ".."
+        break
+      case "Wednesday":
+        return closedOrOpened(schedules.tuesday.opensAt, schedules.tuesday.closesAt)
+        break
+      case "Thursday":
+        return closedOrOpened(schedules.tuesday.opensAt, schedules.tuesday.closesAt)
+        break
+      case "Friday":
+        return closedOrOpened(schedules.friday.opensAt, schedules.friday.closesAt)
+        break
+      case "Saturday":
+        return closedOrOpened(schedules.tuesday.opensAt, schedules.tuesday.closesAt)
+        break
+      case "Sunday":
+        return closedOrOpened(schedules.tuesday.opensAt, schedules.tuesday.closesAt)
+        break
+      default:
+        return null
+    }
+  }
+
+  function closedOrOpened(scheduleDayOpensAt: string, scheduleDayClosesAt: string) {
+    const currentHours = new Date().getHours()
+    const openHours = Number(scheduleDayOpensAt.split(":")[0])
+    const closeHours = Number(scheduleDayClosesAt.split(":")[0])
+    console.log(openHours, closeHours, currentHours)
+    return (currentHours >= openHours && currentHours < closeHours) ? `fecha às ${scheduleDayClosesAt}` : `Abre às ${scheduleDayOpensAt}`
+  }
 
   return (
     <div className="ml-3">
-      <div className="font-bold text-xl">{ restaurant.name }</div>
-      <p className="text-gray-700 text-base"> {restaurant.type} - {distance} km</p>
+      <div className="font-bold text-xl">{name}</div>
+      <Link target={"_blank"} href={"https://www.google.com/maps/place/Estreito,+Petrol%C3%A2ndia+-+State+of+Pernambuco,+56460-000/@-8.749198,-38.260343,15z/data=!3m1!4b1!4m5!3m4!1s0x709bc190fcc0b2b:0x5bb93f4caa4bf8ed!8m2!3d-8.7491958!4d-38.250044"}>
+        <span className="text-gray-700 text-base "> {type} - </span> <span className="text-blue-500">{distance} km</span>
+      </Link>
+      <div className={`h-[20px] w-[108px] rounded flex items-center justify-center shadow-sm mt-3`}>
+        <span className={`font-normal text-sm ${getDailySchadule()![0] == "f" ? 'text-green-500' : 'text-red-500'}`}>{getDailySchadule()}</span>
+      </div>
     </div>
   );
 }
--- src/types/home.ts
+++ src/types/home.ts
@@ -1,7 +1,23 @@
+
 export interface IRestaurant {
     id: string,
     name: string,
     type: string,
+    addressLink: SVGStringList
+    schedules: {
+      monday: IHours
+      tuesday: IHours
+      wednesday: IHours
+      thursday: IHours
+      friday: IHours
+      saturday: IHours
+      sunday: IHours
+    },
     latitude: number,
     longitude: number,
+}
+
+export interface IHours {
+    opensAt: string
+    closesAt: string
 }

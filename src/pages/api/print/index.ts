8bec0742258f2ea5c75dee641e461014836b115e
Eduardo Barroso <barrosoeduardo64@gmail.com>
2023-03-06 21:48:48 -0300

    novo teste de impressão

--- src/pages/api/print/index.ts
+++ src/pages/api/print/index.ts
@@ -1,15 +1,13 @@
 
 import { NextApiRequest, NextApiResponse } from "next";
 import { ThermalPrinter, PrinterTypes, CharacterSet, BreakLine } from 'node-thermal-printer';
-import Printer from 'node-thermal-printer';
 
 let printer = new ThermalPrinter({
-    type: PrinterTypes.STAR,                                  // Printer type: 'star' or 'epson'
-    interface: 'tcp://xxx.xxx.xxx.xxx',                       // Printer interface                      // Printer character set - default: SLOVENIA
-    removeSpecialCharacters: false,                           // Removes special characters - default: false
-    lineCharacter: "=",                                       // Set character for lines - default: "-"
-    options: {                                                 // Additional options
-        timeout: 5000                                           // Connection timeout (ms) [applicable only for network printers] - default: 3000
+    interface: 'tcp://xxx.xxx.xxx.xxx',
+    removeSpecialCharacters: false,
+    lineCharacter: "=",
+    options: {
+        timeout: 5000
     }
 });
 

628b46c7fe1fa619d51912cff2b76e72467936cf
Lucas Bezerra <1lcs.bzrr@gmail.com>
2023-03-23 17:47:56 -0300

    -

--- src/components/DigitalMenuModals/DigitalMenuModals.tsx
+++ src/components/DigitalMenuModals/DigitalMenuModals.tsx
@@ -1,11 +1,21 @@
 import { DigitalMenuContext } from '@/src/contexts/DigitalMenuContext';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import Cart from '../Cart';
 import ProductModal from '../home/ProductModal';
 import { WeekdayOperatingTime } from '../WeekdayOperatingTime';
 
 export default function DigitalMenuModals() {
     const { modals, selectedProduct } = useContext(DigitalMenuContext);
+
+    const body = document.querySelector('body');
+
+    useEffect(() => {
+        if (modals?.state.checkout === true) {
+            body?.classList.add('overflow-hidden');
+        } else {
+            body?.classList.remove('overflow-hidden');
+        }
+    }, [modals?.state.checkout, body?.classList]);
 
     return (
         <>

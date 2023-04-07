548cb77ba3552d0710d67492d71f4c9f014d6cb8
Yan Ueno <yanuenopivot@gmail.com>
2023-04-07 14:07:18 -0300

    modal dinamico

---
+++ src/components/Modal/ModalFooter.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface ModalFooterProps {
+  children: React.ReactNode;
+}
+
+const ModalFooter: React.FC<ModalFooterProps> = ({ children }) => {
+  return <div className="flex space-x-2 justify-end p-6 border-gray-200 rounded-b dark:border-gray-600">{children}</div>;
+};
+
+export default ModalFooter;
--- src/pages/[slug]/admin/configuracoes/TaxaPorDistancia.tsx
+++ src/pages/[slug]/admin/configuracoes/TaxaPorDistancia.tsx
@@ -1,4 +1,7 @@
 /* eslint-disable react-hooks/rules-of-hooks */
+import Modal from '@/src/components/Modal';
+import ModalBody from '@/src/components/Modal/ModalBody';
+import ModalBottom from '@/src/components/Modal/ModalFooter';
 import { AdminContext } from '@/src/contexts/adminContext';
 import { supabase } from '@/src/server/api';
 import React from 'react';
@@ -167,65 +170,26 @@ export default function TaxaPorDistancia() {
                     </tbody>
                 </table>
             </div>
-            {/* Main modal */}
-            <div
-                id="default-modal"
-                data-modal-show={showModal}
-                aria-hidden={!showModal}
-                className={`${
-                showModal ? "" : "hidden"
-                } overflow-x-hidden overflow-y-auto fixed h-modal inset-0 z-50 justify-center items-center flex bg-black bg-opacity-20`}
-            >
-                <div className=" w-full max-w-2xl px-4 h-auto">
-                    <form action="" onSubmit={(e)=>{ e.preventDefault(); handleAddDeliveryFee() }}>
-                        <div className="bg-white rounded-lg shadow relative">
-                            {/* Modal header */}
-                            <div className="flex items-start justify-between p-5 rounded-t dark:border-gray-600">
-                                <h3 className="text-gray-900 text-xl lg:text-2xl font-semibold">
-                                    Taxas de entrega por distância
-                                </h3>
-                                <button
-                                    type="button"
-                                    className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                                    onClick={toggleModal}
-                                >
-                                    <svg
-                                    className="w-5 h-5"
-                                    fill="currentColor"
-                                    viewBox="0 0 20 20"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    >
-                                    <path
-                                        fillRule="evenodd"
-                                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                        clipRule="evenodd"
-                                    ></path>
-                                    </svg>
-                                </button>
-                            </div>
-                            {/* Modal body */}
-                            <div className="p-6 space-y-4 space">
-                                <div>
-                                    <label htmlFor="x0" className="text-sm text-navy-700 font-bold">Digite a distância inicial da taxa de entrega (em km)</label>
-                                    <input type="number" required value={startKm} onChange={(el)=> setStartKm(el.target.value)} className="w-full rounded-lg border border-gray-400 p-2 mt-2" placeholder="Digite a distância inicial ..." />
-                                </div>
-                                <div>
-                                    <label htmlFor="xf" className="text-sm text-navy-700 font-bold">Digite a distância final da taxa de entrega (em km)</label>
-                                    <input type="number" required value={endKm} onChange={(el)=> setEndKm(el.target.value)} className="w-full rounded-lg border border-gray-400 p-2 mt-2" placeholder="Digite a distância final ..." />
-                                </div>
-                                <div>
-                                    <label htmlFor="email" className="text-sm text-navy-700 font-bold ">Digite o valor da taxa de entrega (em R$)</label>
-                                    <input type="number" required value={fee} onChange={(el)=> setFee(el.target.value)} className="w-full rounded-lg border border-gray-400 p-2 mt-2" placeholder="Digite o valor da taxa ..." />
-                                </div>
-                            </div>
-                            <div className="flex space-x-2 justify-end p-6 border-gray-200 rounded-b dark:border-gray-600">
-                                <button data-modal-toggle="default-modal" type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cadastrar</button>
-                                <button data-modal-toggle="default-modal" type="button" onClick={toggleModal} className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600">Cancelar</button>
-                            </div>
-                        </div>    
-                    </form>
-                </div>
-            </div>
+            <Modal title="Taxas de entrega por distância" showModal={showModal} closeFunction={toggleModal}>
+                <Modal.Body>
+                    <div>
+                        <label htmlFor="x0" className="text-sm text-navy-700 font-bold">Digite a distância inicial da taxa de entrega (em km)</label>
+                        <input type="number" required value={startKm} onChange={(el)=> setStartKm(el.target.value)} className="w-full rounded-lg border border-gray-400 p-2 mt-2" placeholder="Digite a distância inicial ..." />
+                    </div>
+                    <div>
+                        <label htmlFor="xf" className="text-sm text-navy-700 font-bold">Digite a distância final da taxa de entrega (em km)</label>
+                        <input type="number" required value={endKm} onChange={(el)=> setEndKm(el.target.value)} className="w-full rounded-lg border border-gray-400 p-2 mt-2" placeholder="Digite a distância final ..." />
+                    </div>
+                    <div>
+                        <label htmlFor="email" className="text-sm text-navy-700 font-bold ">Digite o valor da taxa de entrega (em R$)</label>
+                        <input type="number" required value={fee} onChange={(el)=> setFee(el.target.value)} className="w-full rounded-lg border border-gray-400 p-2 mt-2" placeholder="Digite o valor da taxa ..." />
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <button data-modal-toggle="default-modal" type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cadastrar</button>
+                    <button data-modal-toggle="default-modal" type="button" onClick={toggleModal} className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600">Cancelar</button>
+                </Modal.Footer>
+            </Modal>
         </>
         
     );

76e31aa9cbcdad023738c92c9efe6f1a666c26c0
Yan Ueno <yanuenopivot@gmail.com>
2023-04-10 13:07:13 -0300

    corrigindo modal e base de inputs

--- src/components/nButton/index.tsx
+++ src/components/nButton/index.tsx
@@ -3,11 +3,16 @@ interface Button {
   Disabled?: true | false | boolean;
   bgColor?: 'red' | 'blue' | string;
   txtColor?: string;
-  OnClick: Function;
+  OnClick?: Function;
   Type?: 'button' | 'submit' | 'reset' | undefined;
 }
 
 export default function Button(props: Button) {
+  function onClickFunc() {
+    if (props.OnClick) {
+      props.OnClick();
+    }
+  }
   return (
     <button
       className={`bg-${props.bgColor || 'blue'}-500 text-${
@@ -15,7 +20,7 @@ export default function Button(props: Button) {
       } py-2 px-4 rounded-md mb-4 mt-4 ml-2 mr-2`}
       disabled={props.Disabled}
       {...props}
-      onClick={() => props.OnClick()}
+      onClick={() => onClickFunc()}
       type={props.Type || 'button'}
     >
       {props.text}
--- src/pages/[slug]/admin/configuracoes/TaxaPorDistancia.tsx
+++ src/pages/[slug]/admin/configuracoes/TaxaPorDistancia.tsx
@@ -1,157 +1,152 @@
 /* eslint-disable react-hooks/rules-of-hooks */
+import TextFiled from '@/src/components/Inputs';
 import Modal from '@/src/components/Modal';
-import ModalBody from '@/src/components/Modal/ModalBody';
-import ModalBottom from '@/src/components/Modal/ModalFooter';
+import Button from '@/src/components/nButton';
 import { AdminContext } from '@/src/contexts/adminContext';
 import { supabase } from '@/src/server/api';
-import React from 'react';
 import { useContext, useEffect, useState } from 'react';
 
 export default function TaxaPorDistancia() {
-    const { restaurant } = useContext(AdminContext);
+  const { restaurant } = useContext(AdminContext);
 
-    const [deliveryFees, setDeliveryFees] = useState<any>([]);
+  const [deliveryFees, setDeliveryFees] = useState<any>([]);
 
-    useEffect(() => {
-        const fetchDeliveryFees = async () => {
-            const { data: deliveryFees, error } = await supabase
-                .from('delivery_fees')
-                .select('*')
-                .eq('restaurant_id', restaurant!.id)
-                .order('start_km', { ascending: true });
+  useEffect(() => {
+    const fetchDeliveryFees = async () => {
+      const { data: deliveryFees, error } = await supabase
+        .from('delivery_fees')
+        .select('*')
+        .eq('restaurant_id', restaurant!.id)
+        .order('start_km', { ascending: true });
 
-            if (error) {
-                console.error(error);
-            } else {
-                setDeliveryFees(deliveryFees);
-            }
-        };
+      if (error) {
+        console.error(error);
+      } else {
+        setDeliveryFees(deliveryFees);
+      }
+    };
 
-        fetchDeliveryFees();
-    }, [restaurant]);
+    fetchDeliveryFees();
+  }, [restaurant]);
 
-    if (!restaurant) {
-        return null;
+  if (!restaurant) {
+    return null;
+  }
+
+  const addDeliveryFee = async (
+    startKm: number,
+    endKm: number,
+    fee: number
+  ) => {
+    try {
+      const { data: newDeliveryFee, error } = await supabase
+        .from('delivery_fees')
+        .insert({
+          fee: fee,
+          start_km: startKm,
+          end_km: endKm,
+          restaurant_id: restaurant.id,
+        })
+        .select('*');
+
+      newDeliveryFee
+        ? setDeliveryFees([...deliveryFees, newDeliveryFee[0]])
+        : null;
+    } catch (err) {
+      console.error(err);
     }
+  };
 
-    const addDeliveryFee = async (
-        startKm: number,
-        endKm: number,
-        fee: number
-    ) => {
-        try {
-            const { data: newDeliveryFee, error } = await supabase
-                .from('delivery_fees')
-                .insert({
-                    fee: fee,
-                    start_km: startKm,
-                    end_km: endKm,
-                    restaurant_id: restaurant.id,
-                })
-                .select('*');
+  const deleteDeliveryFee = async (id: number) => {
+    const { error } = await supabase
+      .from('delivery_fees')
+      .delete()
+      .match({ id });
+    if (error) console.error(error);
+    else setDeliveryFees(deliveryFees.filter((fee: any) => fee.id !== id));
+  };
+  const [startKm, setStartKm] = useState<string>();
+  const [endKm, setEndKm] = useState<string>();
+  const [fee, setFee] = useState<string>();
 
-            newDeliveryFee
-                ? setDeliveryFees([...deliveryFees, newDeliveryFee[0]])
-                : null;
-        } catch (err) {
-            console.error(err);
-        }
-    };
-
-    const deleteDeliveryFee = async (id: number) => {
-        const { error } = await supabase
-            .from('delivery_fees')
-            .delete()
-            .match({ id });
-        if (error) console.error(error);
-        else setDeliveryFees(deliveryFees.filter((fee: any) => fee.id !== id));
-    };
-    const [startKm, setStartKm] = useState<string>();
-    const [endKm, setEndKm] = useState<string>();
-    const [fee, setFee] = useState<string>();
-        
-    const handleAddDeliveryFee = () => {
-        // const startKm = parseInt(
-        //     prompt('Digite a distância inicial da taxa de entrega (em km):') ||
-        //         '0'
-        // );
-        // const endKm = parseInt(
-        //     prompt('Digite a distância final da taxa de entrega (em km):') ||
-        //         '0'
-        // );
-        // const fee = parseFloat(
-        //     prompt('Digite o valor da taxa de entrega (em R$):') || '0'
-        // );
-        if (startKm && endKm && fee) {
-            addDeliveryFee(parseFloat(startKm), parseFloat(endKm), parseFloat(fee));
-        }
-        toggleModal();
-        clearInputs();
-    };
-    function clearInputs(){
-        setStartKm("");
-        setEndKm("");
-        setFee("");
+  const handleAddDeliveryFee = () => {
+    // const startKm = parseInt(
+    //     prompt('Digite a distância inicial da taxa de entrega (em km):') ||
+    //         '0'
+    // );
+    // const endKm = parseInt(
+    //     prompt('Digite a distância final da taxa de entrega (em km):') ||
+    //         '0'
+    // );
+    // const fee = parseFloat(
+    //     prompt('Digite o valor da taxa de entrega (em R$):') || '0'
+    // );
+    if (startKm && endKm && fee) {
+      addDeliveryFee(parseFloat(startKm), parseFloat(endKm), parseFloat(fee));
     }
-    const handleDeleteDeliveryFee = (id: number) => {
-        if (
-            window.confirm(
-                'Tem certeza que deseja excluir essa taxa de entrega?'
-            )
-        ) {
-            deleteDeliveryFee(id);
-        }
-    };
+    toggleModal();
+    clearInputs();
+  };
+  function clearInputs() {
+    setStartKm('');
+    setEndKm('');
+    setFee('');
+  }
+  const handleDeleteDeliveryFee = (id: number) => {
+    if (
+      window.confirm('Tem certeza que deseja excluir essa taxa de entrega?')
+    ) {
+      deleteDeliveryFee(id);
+    }
+  };
 
-    const [showModal, setShowModal] = useState(false);
-    const toggleModal = () => {
-        setShowModal(!showModal);
-        clearInputs();
-    };
-    
-    return (
-        <>
-            <div className="p-4">
-                <h1 className="text-2xl font-bold mb-4">
-                    Taxas de entrega por distância
-                </h1>
-                <div className="w-full text-right">
-                    <button
-                        className="bg-blue-500 text-white py-2 px-4 rounded-md mb-4 mt-8"
-                        onClick={toggleModal}
-                    >
-                        Adicionar nova taxa de entrega
-                    </button>
-                </div>
-                <table className="w-full table-auto">
-                    <thead>
-                        <tr>
-                            <th className="px-4 py-2">Distância</th>
-                            <th className="px-4 py-2">Taxa de entrega</th>
-                            <th className="px-4 py-2">Ações</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {deliveryFees.map((deliveryFee: any) => (
-                            <tr key={deliveryFee.id}>
-                                <td className="border px-4 py-2">{`${deliveryFee.start_km} km - ${deliveryFee.end_km} km`}</td>
-                                <td className="border px-4 py-2">{`R$ ${deliveryFee.fee.toFixed(
-                                    2
-                                )}`}</td>
-                                <td className="border px-4 py-2 text-center">
-                                    <button
-                                        className="bg-red-500 text-white py-2 px-4 ml-3 mr-3 rounded-md"
-                                        onClick={() =>
-                                            deliveryFee.id
-                                                ? handleDeleteDeliveryFee(
-                                                    deliveryFee.id
-                                                )
-                                                : console.error('id não encontrado')
-                                        }
-                                    >
-                                        Excluir
-                                    </button>
-                                    {/* <button
+  const [showModal, setShowModal] = useState(false);
+  const toggleModal = () => {
+    setShowModal(!showModal);
+    clearInputs();
+  };
+
+  return (
+    <>
+      <div className="p-4">
+        <h1 className="text-2xl font-bold mb-4">
+          Taxas de entrega por distância
+        </h1>
+        <div className="w-full text-right">
+          <button
+            className="bg-blue-500 text-white py-2 px-4 rounded-md mb-4 mt-8"
+            onClick={toggleModal}
+          >
+            Adicionar nova taxa de entrega
+          </button>
+        </div>
+        <table className="w-full table-auto">
+          <thead>
+            <tr>
+              <th className="px-4 py-2">Distância</th>
+              <th className="px-4 py-2">Taxa de entrega</th>
+              <th className="px-4 py-2">Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            {deliveryFees.map((deliveryFee: any) => (
+              <tr key={deliveryFee.id}>
+                <td className="border px-4 py-2">{`${deliveryFee.start_km} km - ${deliveryFee.end_km} km`}</td>
+                <td className="border px-4 py-2">{`R$ ${deliveryFee.fee.toFixed(
+                  2
+                )}`}</td>
+                <td className="border px-4 py-2 text-center">
+                  <button
+                    className="bg-red-500 text-white py-2 px-4 ml-3 mr-3 rounded-md"
+                    onClick={() =>
+                      deliveryFee.id
+                        ? handleDeleteDeliveryFee(deliveryFee.id)
+                        : console.error('id não encontrado')
+                    }
+                  >
+                    Excluir
+                  </button>
+                  {/* <button
                                         className=" bg-blue-700 text-white py-2 px-4 ml-3 mr-3 rounded-md"
                                         onClick={() =>
                                             deliveryFee.id
@@ -163,38 +158,57 @@ export default function TaxaPorDistancia() {
                                     >
                                         Editar
                                     </button> */}
-                                    
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-            <Modal title="Taxas de entrega por distância" showModal={showModal} closeFunction={toggleModal}>
-                <Modal.Body>
-                    <div>
-                        <label htmlFor="x0" className="text-sm text-navy-700 font-bold">Digite a distância inicial da taxa de entrega (em km)</label>
-                        <input type="number" required value={startKm} onChange={(el)=> setStartKm(el.target.value)} className="w-full rounded-lg border border-gray-400 p-2 mt-2" placeholder="Digite a distância inicial ..." />
-                    </div>
-                    <div>
-                        <label htmlFor="xf" className="text-sm text-navy-700 font-bold">Digite a distância final da taxa de entrega (em km)</label>
-                        <input type="number" required value={endKm} onChange={(el)=> setEndKm(el.target.value)} className="w-full rounded-lg border border-gray-400 p-2 mt-2" placeholder="Digite a distância final ..." />
-                    </div>
-                    <div>
-                        <label htmlFor="email" className="text-sm text-navy-700 font-bold ">Digite o valor da taxa de entrega (em R$)</label>
-                        <input type="number" required value={fee} onChange={(el)=> setFee(el.target.value)} className="w-full rounded-lg border border-gray-400 p-2 mt-2" placeholder="Digite o valor da taxa ..." />
-                    </div>
-                </Modal.Body>
-                <Modal.Footer>
-                    <button data-modal-toggle="default-modal" type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cadastrar</button>
-                    <button data-modal-toggle="default-modal" type="button" onClick={toggleModal} className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600">Cancelar</button>
-                </Modal.Footer>
-            </Modal>
-        </>
-        
-    );
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <Modal
+        title="Taxas de entrega por distância"
+        showModal={showModal}
+        closeFunction={toggleModal}
+        onSubmitFunction={handleAddDeliveryFee}
+      >
+        <Modal.Body>
+          <TextFiled
+            required
+            Type="number"
+            value={startKm}
+            setValue={setStartKm}
+            placeHolder="Digite a distância inicial da taxa de entrega (em km)"
+            label="Digite a distância inicial da taxa de entrega (em km)"
+          />
+          <TextFiled
+            required
+            Type="number"
+            value={endKm}
+            setValue={setEndKm}
+            placeHolder="Digite a distância final ..."
+            label="Digite a distância final da taxa de entrega (em km)"
+          />
+          <TextFiled
+            required
+            Type="number"
+            value={fee}
+            setValue={setFee}
+            placeHolder="Digite o valor da taxa ..."
+            label="Digite o valor da taxa de entrega (em R$)"
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button Type="submit" text="Cadastrar" />
+          <Button
+            Type="button"
+            bgColor="red"
+            text="Cancelar"
+            OnClick={toggleModal}
+          />
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
 function preventDefault() {
-    throw new Error('Function not implemented.');
+  throw new Error('Function not implemented.');
 }
-

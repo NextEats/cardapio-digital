466600a57dfb424691466655fc5793f05d7fcc7b
Eduardo Barroso <barrosoeduardo64@gmail.com>
2023-04-14 18:23:23 -0300

    Calculo do valor total gasto na mesa

--- src/components/admin/Tables/Table/Payment/PaymentContent.tsx
+++ src/components/admin/Tables/Table/Payment/PaymentContent.tsx
@@ -1,13 +1,19 @@
+import { TableContext } from '@/src/contexts/TableContext';
+import { useContext } from 'react';
 import ValueCard from '../feito/ValueCard';
 import { PaymentsTable } from './PaymentsTable';
 
 interface iPaymentContentProps {}
 
 export function PaymentContent({}: iPaymentContentProps) {
+  const { orders_products } = useContext(TableContext);
+  const totalSpent = orders_products.reduce((acc, item) => {
+    return (acc = item.total_price * item.quantity);
+  }, 0);
   return (
     <div className="h-[calc(100%-80px)] px-16 py-4 flex flex-col 2md:flex-row gap-3 lg:gap-10">
       <div className="flex 2md:flex-col gap-3 p-3">
-        <ValueCard title="Total gasto" value={'300,00'} />
+        <ValueCard title="Falta" value={`${totalSpent}`} />
         <ValueCard
           title="Total pago"
           value={'300,00'}
--- src/components/admin/Tables/Table/feito/TableValueCards.tsx
+++ src/components/admin/Tables/Table/feito/TableValueCards.tsx
@@ -1,9 +1,17 @@
+import { TableContext } from '@/src/contexts/TableContext';
+import { useContext } from 'react';
 import ValueCard from './ValueCard';
 
 export default function TableValueCards() {
+  const { orders_products } = useContext(TableContext);
+
+  const totalSpent = orders_products.reduce((acc, item) => {
+    return (acc = item.total_price * item.quantity);
+  }, 0);
+
   return (
     <div className="flex 2md:flex-col gap-3 p-3">
-      <ValueCard title="Total gasto" value={'300,00'} />
+      <ValueCard title="Total gasto" value={`${totalSpent}`} />
       <ValueCard
         title="Total pago"
         value={'300,00'}

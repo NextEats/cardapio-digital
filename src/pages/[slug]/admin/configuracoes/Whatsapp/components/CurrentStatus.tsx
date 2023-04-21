a9ae8acbdd328fc488966341b6066ab20104bbf3
Lucas Bezerra <1lcs.bzrr@gmail.com>
2023-04-21 19:39:30 -0300

    fix build errors

--- src/pages/[slug]/admin/configuracoes/Whatsapp/components/CurrentStatus.tsx
+++ src/pages/[slug]/admin/configuracoes/Whatsapp/components/CurrentStatus.tsx
@@ -1,37 +1,37 @@
-import { tVenomStatus } from '../Whatsapp';
+// import { tVenomStatus } from '../Whatsapp';
 
-interface iCurrentStatus {
-  status: tVenomStatus;
-}
+// interface iCurrentStatus {
+//   status: tVenomStatus;
+// }
 
-const CurrentStatus: React.FC<iCurrentStatus> = props => {
-  const { status } = props;
+// const CurrentStatus: React.FC<iCurrentStatus> = props => {
+//   const { status } = props;
 
-  const StatusText = () => {
-    switch (status) {
-      case 'successChat':
-        return <>Conectado</>;
-      default:
-        return <>Carregando...</>;
-    }
-  };
+//   const StatusText = () => {
+//     switch (status) {
+//       case 'successChat':
+//         return <>Conectado</>;
+//       default:
+//         return <>Carregando...</>;
+//     }
+//   };
 
-  const returnBackgroundColor = () => {
-    switch (status) {
-      case 'successChat':
-        return 'bg-green-600';
-      default:
-        return 'bg-orange-600';
-    }
-  };
+//   const returnBackgroundColor = () => {
+//     switch (status) {
+//       case 'successChat':
+//         return 'bg-green-600';
+//       default:
+//         return 'bg-orange-600';
+//     }
+//   };
 
-  return (
-    <div
-      className={`rounded max-w-[400px] p-4 text-center uppercase font-semibold text-white ${returnBackgroundColor()}`}
-    >
-      <StatusText />
-    </div>
-  );
-};
+//   return (
+//     <div
+//       className={`rounded max-w-[400px] p-4 text-center uppercase font-semibold text-white ${returnBackgroundColor()}`}
+//     >
+//       <StatusText />
+//     </div>
+//   );
+// };
 
-export default CurrentStatus;
+// export default CurrentStatus;

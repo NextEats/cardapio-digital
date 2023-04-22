1f33d22c138c5fa074765bfed2ab8d1a9ea27760
Lucas Bezerra | React & React Native <48138924+bezerra-lucas@users.noreply.github.com>
2023-04-22 14:57:56 +0000

    teste 2

--- src/pages/api/whatsapp/check-status.ts
+++ src/pages/api/whatsapp/check-status.ts
@@ -10,10 +10,15 @@ export default async function tables(
 
   switch (method) {
     case 'POST':
-      const { data } = await whatsappRestApi.post('/check-status', {
-        id: slug,
-      });
-      res.status(200).json(data);
+      try {
+        const { data } = await whatsappRestApi.post('/check-status', {
+          id: slug,
+        });
+
+        res.status(200).json(data);
+      } catch (error) {
+        console.error(error);
+      }
       break;
     default:
       res.status(404).end();

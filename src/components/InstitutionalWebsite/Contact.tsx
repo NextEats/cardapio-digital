9a732c785906c0d91f9ba8cc6bc06a75d272d20f
Edaurdo <barrosoeduardo64@gmail.com>
2023-05-08 15:17:32 -0300

    ..

--- src/components/InstitutionalWebsite/Contact.tsx
+++ src/components/InstitutionalWebsite/Contact.tsx
@@ -121,7 +121,7 @@ export default function Contact() {
               onClick={() => handleSendEmail()}
               className="md:w-32 bg-indigo-600 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-indigo-500 transition ease-in-out duration-300"
             >
-              ENVIAR
+              ENVIARs
             </button>
           </form>
         </div>

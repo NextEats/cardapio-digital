961fa40f0a78b06d37d4542e4084c8cc35203570
brenoguerra <brenoguerradev@gmail.com>
2023-05-17 01:58:06 -0300

    chore: texto do label para light

--- src/components/pages/CreateRestaurant/input.tsx
+++ src/components/pages/CreateRestaurant/input.tsx
@@ -9,7 +9,7 @@ interface iInputProps extends InputHTMLAttributes<HTMLInputElement> {
 export function Input({ label, ...rest }: iInputProps) {
   return (
     <div className="flex flex-col">
-      <label className="text-gray-400 font-medium">{label}</label>
+      <label className="text-gray-400 font-light">{label}</label>
       <input className="mt-1 w-full h-[36px] py-[10px] px-[12px] bg-white .placeholder-gray-300 border-2 border-gray-200 rounded outline-none focus:border focus:border-brand-dark-orange" {...rest}></input>
     </div>
   )

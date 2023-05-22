76a3128b3bdcb41a99758ceda4bcbba88ead8b9f
Edaurdo <barrosoeduardo64@gmail.com>
2023-05-22 13:35:39 -0300

    feat: inicio da criação do página de time

---
+++ src/pages/[slug]/admin/team/index.tsx
@@ -0,0 +1,13 @@
+import AdminWrapper from '@/src/components/admin/AdminWrapper';
+
+export default function team() {
+  return (
+    <>
+      <AdminWrapper>
+        <div>
+          <h1>Hello Team</h1>
+        </div>
+      </AdminWrapper>
+    </>
+  );
+}
